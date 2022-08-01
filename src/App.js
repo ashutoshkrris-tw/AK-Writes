12384fd80ba52e945982db17aa21ed630edd4670
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-08-01 23:57:52 +0530

    Use Router and Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreateBlog from "./components/Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateBlog />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    const abortController = new AbortController();
+
+    fetch(url, { signal: abortController.signal })
       .then((response) => {
         if (!response.ok) {
           throw Error("Unable to fetch data from the server");
@@ -19,9 +21,13 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((error) => {
-        setError(error.message);
-        setisPending(false);
+        if (error.name === "AbortError") console.log("Fetch aborted");
+        else {
+          setError(error.message);
+          setisPending(false);
+        }
       });
+    return () => abortController.abort();
   }, [url]);
   return { data, isPending, error };
 };
